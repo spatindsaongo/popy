056ab0c84975afaa7a3d91a8991bcd91a958bb7d
SAM Patindsaongo <spatindsaongo@gmail.com>
2012-09-05 15:21:56 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Popy::Application.config.secret_token = '951d558ed4963df791e6f90f6a912a63fc56dd7c72946ee488427b37d8098911612745bd74e13452443b1d804023851b438debf139c8f7a3461bce021c0cd118'
