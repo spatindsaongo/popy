63a0f79ef34e7cba09a02af9e8d322011335d5f9
SAM Patindsaongo <spatindsaongo@gmail.com>
2012-09-10 16:51:17 +0000

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,59 +1,34 @@
 require 'spec_helper'
 
 describe "Static pages" do
-
+  subject { page }
+  
   describe "Home page" do
     before { visit root_path}
 
-    it "should have the h1 'Popy App'" do
-      page.should have_selector('h1', :text => 'Popy App')
-    end
-
-    it "should have the title 'Home'" do
-      page.should have_selector('title',
-			        :text => "Popy")
-    end
-    
-    it "should not have a custom page title" do
-      page.should_not have_selector('title', :text => '| Home')
-    end
+    it { should have_selector('h1', :text => 'Popy App')}
+    it {should have_selector('title', :text => full_title(''))}    
+    it {page.should_not have_selector('title', :text => '| Home')}
   end
 
   describe "Help" do
     before { visit help_path}
     
-    it "should have the h1 'Help'" do
-      page.should have_selector('h1', :text => 'Help')
-    end
-
-    it "should have the title 'Help'"do
-      page.should have_selector('title',
-			        :text => "Popy | Help")
-    end
+    it { should have_selector('h1', :text => 'Help') }
+    it {should have_selector('title', :text => full_title('Help'))}
   end
 
   describe "About page" do
     before { visit about_path}
     
-    it "should have the h1 'About Us'" do
-      page.should have_selector('h1', :text => 'About Us')
-    end
-
-    it "should have the title 'About Us'" do
-      page.should have_selector('title',
-			        :text => "Popy | About Us")
-    end
+    it { should have_selector('h1', :text => 'About') }
+    it {should have_selector('title', :text => full_title('About Us'))}
   end
   
   describe "Contact page" do
     before { visit contact_path}
     
-    it "should have the h1 'Contact'" do
-      page.should have_selector('h1', :text => 'Contact')
-    end
-    
-    it "should have the title 'Contact'" do
-      page.should have_selector('title', :text => 'Popy | Contact')
-    end
+    it { should have_selector('h1', :text => 'Contact') }
+    it {should have_selector('title', :text => full_title('Contact'))}
   end
 end
