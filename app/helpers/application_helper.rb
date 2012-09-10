b1926624c0ab3558fbf0cfa2d8d4424591f5d767
SAM Patindsaongo <spatindsaongo@gmail.com>
2012-09-10 15:47:13 +0000

    Finnished with route definition

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  #retourne le titre complet d'une page
+  def full_title(page_title)
+    base_title = "Popy"    
+    return "#{base_title} | #{page_title}" unless page_title.empty?
+    return base_title
+  end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,26 +5,31 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the h1 'Popy App'" do
-      visit  '/static_pages/home'
+      visit  root_path
       page.should have_selector('h1', :text => 'Popy App')
     end
 
-    it "should have the title 'Home'"do
-      visit '/static_pages/home'
+    it "should have the title 'Home'" do
+      visit root_path
       page.should have_selector('title',
-			        :text => "Popy | Home")
+			        :text => "Popy")
+    end
+    
+    it "should not have a custom page title" do
+      visit root_path
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
 
   describe "Help" do
 
     it "should have the h1 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('h1', :text => 'Help')
     end
 
     it "should have the title 'Help'"do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('title',
 			        :text => "Popy | Help")
     end
@@ -33,14 +38,27 @@ describe "Static pages" do
   describe "About page" do
 
     it "should have the h1 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('h1', :text => 'About Us')
     end
 
     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('title',
 			        :text => "Popy | About Us")
+    end
+  end
+  
+  describe "Contact page" do
+    
+    it "should have the h1 'Contact'" do
+      visit contact_path
+      page.should have_selector('h1', :text => 'Contact')
+    end
+    
+    it "should have the title 'Contact'" do
+      visit contact_path
+      page.should have_selector('title', :text => 'Popy | Contact')
     end
   end
 end
