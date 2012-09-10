f7f950ab1677b9145c11d4df1d5a5a596750f059
SAM Patindsaongo <spatindsaongo@gmail.com>
2012-09-10 18:26:13 +0000

    Modification made on spec's files

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,6 +1,3 @@
 #retourne le titre complet d'une page
-def full_title(page_title)
-  base_title = "Popy"    
-  return "#{base_title} | #{page_title}" unless page_title.empty?
-  return base_title
-end
+
+include ApplicationHelper
