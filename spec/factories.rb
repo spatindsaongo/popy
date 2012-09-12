e6bb7b819639a3a683a77f73095f19427897dafb
SAM Patindsaongo <spatindsaongo@gmail.com>
2012-09-12 17:03:43 +0000

    Finnish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Popy Coyotte"
+    email    "popy.coyotte@aosis.ld"
+    password "popypass"
+    password_confirmation "popypass"
+  end
+end
